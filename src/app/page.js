0ca4ec19ab55829a68ec99d2e612cfc8236e967c
Rjk-Jami <rjkhan.1221@gmail.com>
2025-03-13 15:48:06 +0600

    add button for state

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,12 @@
+import Authentication from "@/components/authentication/Authentication";
 
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <div className="w-full max-w-md">
+        <h1 className="text-3xl font-bold text-center mb-6">Welcome Back!</h1>
+        <Authentication />
+      </div>
+    </main>
+  );
 }
---
+++ src/components/authentication/Authentication.jsx
@@ -0,0 +1,37 @@
+"use client";
+import React, { use, useState } from "react";
+import Login from "../Login/Login";
+import Registration from "../Registration/Registration";
+
+const Authentication = () => {
+  const [loginActive, setLoginActive] = useState(true);
+  return (
+    <div className="container">
+      <div className=" grid grid-cols-2 justify-center items-center gap-2">
+        <button
+          onClick={() => setLoginActive(!loginActive)}
+          className={`btn btn-outline btn-primary ${
+            loginActive ? "btn-active" : ""
+          } `}
+        >
+          Login
+        </button>
+
+        <button
+          onClick={() => setLoginActive(!loginActive)}
+          className={`btn btn-outline btn-primary ${
+            loginActive ? "" : "btn-active"
+          }`}
+        >
+          Registration
+        </button>
+      </div>
+
+      <div className="">
+      {loginActive ? <Login /> : <Registration />}
+      </div>
+    </div>
+  );
+};
+
+export default Authentication;
