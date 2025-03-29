7ffbbc3327e05c405c2fa7b0d696c0dd124db269
Rjk-Jami <rjkhan.1221@gmail.com>
2025-03-30 02:56:31 +0600

    add hover design on hover

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,10 @@ import Authentication from "@/components/authentication/Authentication";
 export default function Home() {
   return (
     <div className="relative">
-      <section className="absolute m-0 p-0 inset-0 w-full h-full flex justify-around items-center gap-0.5 flex-wrap overflow-hidden -z-0">
+      <section className="absolute m-0 p-0 inset-0 w-full h-full flex justify-around items-center gap-2 flex-wrap overflow-hidden -z-0 ">
       
       {Array.from({ length: 350 }).map((_, i) => (
-        <span key={i} className="relative block hover:bg-red-500 bg-zinc-900"></span>
+        <span key={i} className="relative block hover:bg-red-500 bg-zinc-900 transition hover:duration-75 duration-300 ease-in-out"></span>
       ))}
       
     </section>
