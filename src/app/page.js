b6503c3fee3fcc228e446d3d2f3d0cf18a05b555
Rjk-Jami <rjkhan.1221@gmail.com>
2025-04-12 21:04:12 +0600

    change background color

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,11 @@ import Authentication from "@/components/authentication/Authentication";
 // EID MUBARAK2
 export default function Home() {
   return (
-    <div className="relative">
+    <div className="relative bg-black">
       <section className="absolute m-0 p-0 inset-0 w-full h-full flex justify-around items-center gap-0.5 flex-wrap overflow-hidden -z-0 ">
       
       {Array.from({ length: 350 }).map((_, i) => (
-        <span key={i} className="relative block hover:bg-[#f00]/80 bg-zinc-900 transition hover:duration-75 duration-300 ease-in-out"></span>
+        <span key={i} className="relative block hover:bg-transparent bg-zinc-900 transition hover:duration-100 duration-300 ease-in-out"></span>
       ))}
       
     </section>
